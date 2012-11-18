534fe4b0378b220d216d3e57a7792d2c70180f45
Michael Schiff <schiff.michael@gmail.com>
2012-11-18 06:06:33 -0800

    i have no idea what is going on.  this look like they work

---
+++ _Fancy/_Fancy/__init__.py
@@ -0,0 +1,7 @@
+# Source Generated with Decompyle++
+# File: __init__.pyc (Python 2.5)
+
+
+def create_instance(c_instance):
+    return 0
+
--- leap/MidiInterface.py
+++ leap/MidiInterface.py
@@ -12,8 +12,9 @@ class MidiInterface():
     def send(self, data):
             self.sock.sendto(data, self.ADDR)
     def set_tempo(self, bpm):
-        x = bpm-20
-        x = int(math.floor(x/7.709))
+        y = 127.0/(999-20)
+        x = y*(bpm-20)
+        x = int(math.floor(x))
         self.send("/tempo %d" % x)
     def stop_track(self, track_num):
         data = "/track%d /stop 127" % track_num
