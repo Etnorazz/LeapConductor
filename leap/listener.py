69db1bad71af20e4de0fa049be861e9c09889b9c
Henry Bradlow <hbradlow@berkeley.edu>
2012-11-17 19:31:53 -0800

    Reformated temp

--- leap/listener.py
+++ leap/listener.py
@@ -1,39 +1,17 @@
 from lib import Leap
 import sys
 from controller import AbletonController
-from tempo import TempoRecognizer
-
-def f(a):
-    pass
-class LeapListener(Leap.Listener):
-    def __init__(self, gestures):
-        super(LeapListener,self).__init__()
-        self.controller = AbletonController(gestures)
-
-    def onInit(self, controller):
-        print "Initialized"
-
-    def onConnect(self, controller):
-        print "Connected"
-
-    def onDisconnect(self, controller):
-        print "Disconnected"
-
-    def onFrame(self,controller):
-        self.controller.dispatch(controller.frame())
 
 def main():
     # Create a sample listener and assign it to a controller to receive events
-    listener = LeapListener(['tempoChange'])
-    controller = Leap.Controller(listener)
+    ableton_controller = AbletonController()
 
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
     sys.stdin.readline()
 
     # The controller must be disposed of before the listener
-    controller = None
+    ableton_controller.destroy()
 
 if __name__=="__main__":
     main()
-
