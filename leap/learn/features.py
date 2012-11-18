77575f13f1f47add5be0f15e15b53e532dd4f9c1
Henry Bradlow <hbradlow@berkeley.edu>
2012-11-18 03:48:08 -0800

    Leaning working well with large traning sets

--- leap/learn/features.py
+++ leap/learn/features.py
@@ -34,9 +34,9 @@ def avgFingers(frames):
     """
         Return the average number of fingers per hand
     """
-    avg_fingers =sum([getNumFingers(frame) for frame in frames]) / sum([getNumHands(frame) for frame in frames])
-    print avg_fingers
-    return avg_fingers
+    avg_fingers = sum([getNumFingers(frame) for frame in frames])  / len(frames)
+    avg_fingers = avg_fingers ** 8
+    return [avg_fingers for i in range(10)]
 
 def fingerVariance(frames):
     """
@@ -116,3 +116,25 @@ def average_velocity(frames):
                 values.append(finger.velocity())
     average_value = utils.average_position(values)
     return [average_value.x,average_value.y,average_value.z]
+
+
+def list_3d(size):
+    return [[[0 for i in range(size)] for j in range(size)] for k in range(size)]
+def velocity_histogram(frames,bins = 4,range=(-1.,1.)):
+    length = range[1]-range[0]
+    bin_size = length/bins
+
+    def hround(v):
+        return min(bins-1,int((v-range[0])/bin_size))
+
+    l = list_3d(bins)
+    for frame in frames:
+        for hand in frame.hands():
+            for finger in hand.fingers():
+                v = finger.velocity()
+                vector = Leap.Vector(v.x,v.y,v.z)
+                x = v.x/utils.norm(vector)
+                y = v.y/utils.norm(vector)
+                z = v.z/utils.norm(vector)
+                l[hround(x)][hround(y)][hround(z)] += 1
+    return l
